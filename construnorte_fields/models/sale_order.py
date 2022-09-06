db5476dcea7686ca9c2903dfd71d97f238f9f43f
Construnorte <106209696+construnorte@users.noreply.github.com>
2022-09-06 16:51:25 -0500

    Update sale_order.py

--- construnorte_fields/models/sale_order.py
+++ construnorte_fields/models/sale_order.py
@@ -6,13 +6,14 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
     precio_promedio = fields.Float('Precio promedio', compute="_precio_promedio", readonly=True, store=True)
-
+    
+    @api.depends('product_uom_qty','price_total')
     def _precio_promedio(self):
         for rec in self:
-            if rec.product_uom_qty > 0:
-                precio_promedio = ( rec.price_total / rec.product_uom_qty )
+            if rec.product_uom_qty is None or rec.product_uom_qty == 0:
+                precio_promedio = ( rec.price_total / 1.0 )
             else: 
-                precio_promedio = 0
+                precio_promedio = ( rec.price_total / rec.product_uom_qty )
             rec.precio_promedio = precio_promedio
 
 class SaleOrder(models.Model):
