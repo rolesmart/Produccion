a8dd3d8cf94230ba30f2cafd63f660a56214b145
Construnorte <106209696+construnorte@users.noreply.github.com>
2023-05-03 00:31:25 -0600

    Update __manifest__.py

--- construnorte_fields/__manifest__.py
+++ construnorte_fields/__manifest__.py
@@ -1,13 +1,12 @@
 # 2022 Staff Informático y Sistemas Computacionales
 # @author Staf <estrategia@staffinformatico.com.mx>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 {
     "name": "Custom fields Construnorte",
     "version": "15.0.2.0.0",
     "category": "Sale",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "summary": "Campos personalizados Construnorte",
     "author": "Rolando Estrada Martínez",
     "depends": ["sale", "purchase",],
